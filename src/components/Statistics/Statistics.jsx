84f6c12fcf22e84c62167e2d439c2b932a2462a9
sofiatuzinska <129293949+sofiatuzinska@users.noreply.github.com>
2023-11-12 16:37:11 +0200

    summary

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import PropTypes from "prop-types";
+import { ItemLabel, ItemPercentage, StatisticsTitle, StatisticsWrapper, StatItem, StatList } from "./Statistics.module";
+import getRandomHexColor from "./GetRandomColor";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const Statistics = ({ title, stats}) => {
+    return (
+        <StatisticsWrapper>
+            {title && <StatisticsTitle>{title}</StatisticsTitle>}
+            
+            <StatList>
+                {stats.map(({id,label,percentage}) => {
+                     return (<StatItem key={id} style={{backgroundColor: getRandomHexColor()}}>
+                        <ItemLabel>{label}</ItemLabel>
+                        <ItemPercentage>{percentage}%</ItemPercentage>
+                    </StatItem>)  
+                 })
+                }
+            </StatList>
+        </StatisticsWrapper>
+    );
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.array
+}
