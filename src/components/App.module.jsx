8d43e9da0b00c4b6fd9e76481e2de356c7162da1
sofiatuzinska <129293949+sofiatuzinska@users.noreply.github.com>
2023-11-12 16:34:53 +0200

    sum

---
+++ src/components/App.module.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  text-align: center;
+  width: 412px;
+  margin: auto;
+`
+export const Wrapper = styled.div`
+
+  @media screen and (min-width: 425px){
+    display: flex;
+    justify-content: space-between
+  }
+`
